35905e9b0c3eb2996e785fdd2e76060d2599eb67
Kamil Dej <kamil.dej@gmail.com>
2022-12-22 02:15:38 +0100

    next upr

--- main.py
+++ main.py
@@ -4,38 +4,45 @@ class Village:
         self.name = name
         self.location = location
         self.population = population
-        if population > self.max_population:
+        if population or self.population > self.max_population:
             raise ValueError(f"Population limit is {self.max_population}")
 
     def __str__(self):
         return f"Village name: {self.name} \nLocation: {self.location} \nPopulation: {self.population}"
 
     def add_population(self):
-        if self.population < self.max_population:
-            self.population += 1
-        else:
-            raise ValueError(f"Max population is {self.max_population}. "
-                             f"You can not add more residents to village {self.name}")
+        self.population += 1
 
 
 class Building:
-    def __init__(self, name, capacity, residents):
+    def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
-        self.residents = residents
 
+    def __str__(self):
+        return f"name: {self.name}, capacity: {self.capacity}"
 
 
 class Workshop(Building):
-    def __init__(self, name, capacity, residents,  manufactured_toys):
-        super().__init__(name, capacity, residents)
+    def __init__(self, name, capacity, workers, manufactured_toys):
+        super().__init__(name, capacity,)
         self.manufactured_toys = manufactured_toys
-        self.working_elves = residents
+        self.working_elves = workers
+
+    def __str__(self):
+        return f"name: {self.name}, capacity: {self.capacity}, working elves: {self.working_elves}" \
+               f"toy production: {self.manufactured_toys} - {Workshop.produce_toys_per_day(self)}quantity per day"
+
+    def produce_toys_per_day(self):
+        hour_production = 30 * self.working_elves
+        day_production = hour_production * 24
+        return day_production
 
 
 class House(Building):
-    def __init__(self, name, capacity, residents, b):
-        super().__init__(name, capacity, residents)
+    def __init__(self, name, capacity, residents):
+        super().__init__(name, capacity)
+        self.residents = residents
 
 
 class Resident:
@@ -44,19 +51,25 @@ class Resident:
         self.age = age
         self.height = height
         self.weight = weight
-        Village.add_population(self)
+        self.add = Village.add_population(self)
 
 
 class Santa(Resident):
-    pass
+    def __int__(self, name, age, height, weight, special_skills=[]):
+        super().__init__(name, age, height, weight)
+        self.special_skill = special_skills
+        self.weight = weight * 1.5
+
+    def distribution_of_presents(self):
+        time = 8
+        number_of_laps_of_the_earth = 4
+        number_of_presents = 7700000000
+        return f"Santa distributed {}"
 
 
 class Elf(Resident):
     pass
 
-
-class MagicElf(Elf):
-    pass
 
 
 class Reindeer(Resident):
